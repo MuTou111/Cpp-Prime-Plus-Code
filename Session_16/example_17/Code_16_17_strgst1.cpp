06b8d76635ad73aa295e0a03bdd63dc4848e7783
WODD_PIGRON <913650440@qq.com>
2023-04-14 21:56:32 +0800

    update

---
+++ Session_16/example_17/Code_16_17_strgst1.cpp
@@ -0,0 +1,23 @@
+// strgst1.cpp -- applying the STL to a string
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+int main()
+{
+    using namespace std;
+    string letters;
+    cout << "Enter the letter grouping (q to quit): ";
+    while (cin >> letters && letters != "quit")
+    {
+        cout << "Permutations of " << letters << endl;
+        sort(letters.begin(), letters.end());
+        cout << letters << endl;
+        while (next_permutation(letters.begin(), letters.end()))
+            cout << letters << endl;
+        cout << "Enter next sequence (q to quit): ";
+    }
+    cout << "Done.\n";
+
+    return 0;
+}
